ab8a081ee9e40d7d355e83bb5158285c6780b673
Yury Kuznetsov <yyk99@kestrel.home>
2026-08-21 09:14:38 -0400

    Add nano/line-styles-demo.py script

---
+++ nano/line-styles-demo.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+#
+# Demo of matplotlib named and parametrized line styles.
+# Useful as a reference when choosing line styles for plot scripts.
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+linestyle_str = [
+     ('solid', 'solid'),      # Same as (0, ()) or '-'
+     ('dotted', 'dotted'),    # Same as ':'
+     ('dashed', 'dashed'),    # Same as '--'
+     ('dashdot', 'dashdot')]  # Same as '-.'
+
+linestyle_tuple = [
+     ('loosely dotted',        (0, (1, 10))),
+     ('dotted',                (0, (1, 5))),
+     ('densely dotted',        (0, (1, 1))),
+
+     ('long dash with offset', (5, (10, 3))),
+     ('loosely dashed',        (0, (5, 10))),
+     ('dashed',                (0, (5, 5))),
+     ('densely dashed',        (0, (5, 1))),
+
+     ('loosely dashdotted',    (0, (3, 10, 1, 10))),
+     ('dashdotted',            (0, (3, 5, 1, 5))),
+     ('densely dashdotted',    (0, (3, 1, 1, 1))),
+
+     ('dashdotdotted',         (0, (3, 5, 1, 5, 1, 5))),
+     ('loosely dashdotdotted', (0, (3, 10, 1, 10, 1, 10))),
+     ('densely dashdotdotted', (0, (3, 1, 1, 1, 1, 1)))]
+
+
+#MARKERS = ['o', 's', '^', 'x', 'D', '+', 'v', '*']
+MARKERS = ['o', 's', 'x']
+
+
+def plot_linestyles(ax, linestyles, title):
+    X, Y = np.linspace(0, 100, 10), np.zeros(10)
+    yticklabels = []
+
+    for i, (name, linestyle) in enumerate(linestyles):
+        marker = MARKERS[i % len(MARKERS)]
+        ax.plot(X, Y+i, linestyle=linestyle, linewidth=1.5, color='black',
+                marker=marker, markersize=10)
+        yticklabels.append(name)
+
+    ax.set_title(title)
+    ax.set(ylim=(-0.5, len(linestyles)-0.5),
+           yticks=np.arange(len(linestyles)),
+           yticklabels=yticklabels)
+    ax.tick_params(left=False, bottom=False, labelbottom=False)
+    ax.spines[:].set_visible(False)
+
+    # For each line style, add a text annotation with a small offset from
+    # the reference point (0 in Axes coords, y tick value in Data coords).
+    for i, (name, linestyle) in enumerate(linestyles):
+        ax.annotate(repr(linestyle),
+                    xy=(0.0, i), xycoords=ax.get_yaxis_transform(),
+                    xytext=(-6, -12), textcoords='offset points',
+                    color="blue", fontsize=11, ha="right", family="monospace")
+
+
+plt.rcParams.update({"font.size": 14})
+
+fig, (ax0, ax1) = plt.subplots(2, 1, figsize=(7, 8), height_ratios=[1, 3],
+                               layout='constrained')
+
+plot_linestyles(ax0, linestyle_str[::-1], title='Named linestyles')
+plot_linestyles(ax1, linestyle_tuple[::-1], title='Parametrized linestyles')
+
+plt.show()
